1eb4c38c7dfc9e3879b73983195890226e121c4d
jackshamama <shamama@gmail.com>
2016-04-14 10:07:48 -0700

    starting 4.3 release 1

---
+++ ruby/hamsters.rb
@@ -0,0 +1,19 @@
+puts "What is the hamster's name?"
+name = gets.chomp
+puts "What is the volume level from 1 to 10?"
+volume = gets.chomp.to_i
+puts "What is the fur color?"
+color = gets.chomp
+puts "Is the hamster a good candidate for adoption?"
+candidate = gets.chomp
+p candidate
+puts "What is the estimated age of your hamster?"
+age = gets.chomp.to_i
+# p age
+# if age is an integer, okay, if nil then nil
+ 	if age >= 1 
+ 	age = age
+ else
+ 	age = nil
+ end
+p age
