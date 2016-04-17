3c5ea35f3536f56cafb72942e5b402d4421bb05a
jackshamama <shamama@gmail.com>
2016-04-17 16:54:51 -0700

    adding vampire ruby code (again)

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,60 +1,55 @@
 puts "How many employees  are there to proces?"
 	num = gets.chomp.to_i
-
 		num.times do
-			puts "What is your name?"
-			name = gets.chomp
+				puts "Enter your name."
+				name = gets.chomp
 
+				puts "How old are you?"
+				age = gets.chomp.to_i
 
-			puts "How old are you?"
-			age1 = gets.chomp.to_i
+				puts "What year were you born?"
+				year_born = gets.chomp.to_i
 
-
-
-			puts "What year were you born?"
-			age2 = gets.chomp.to_i
-
-
-			puts "Would you like some garlic bread from our cafeteria?"
-				garlic = if gets.chomp.downcase == "yes"
-					true	
-					else
-					false
+				puts "Would you like some garlic bread from our cafeteria?"
+					garlic = if gets.chomp.downcase == "yes"
+						true	
+						else 
+						false
 				end
 
-			puts "Do you need health insurance?"
-				insurance = if gets.chomp.downcase == "yes"
-					true
-					else
-					false
+				puts "Do you need health insurance?"
+					insurance = if gets.chomp.downcase == "yes"
+						true
+						else
+						false
 				end
 
-			puts "Name any allergies, and type 'done' when finished."
-				allergy =  gets.chomp.downcase
-					while allergy != "done" 
-					if allergy == "sunshine"
-					puts "Probably a vampire"
-					break
+				puts "Name any allergies, and type 'done' when finished."
+					allergy =  gets.chomp.downcase
+						while allergy != "done" 
+							if allergy == "sunshine"
+							puts "Probably a vampire"
+						break
 				end
 
-			puts "Name any allergies, one at a time and type done when finished."	
-				allergy = gets.chomp.downcase
+				puts "Name any allergies, one at a time and type done when finished."	
+					allergy = gets.chomp.downcase
 				end
 
 					if allergy == "sunshine"
 					next
 
-					elsif (2016 - age2 == age1 && (garlic || insurance)) && name != ("Drake Cula" || "Tu Fang")
-					puts "Probably not a vampire"
+					elsif (2016 - year_born == age && (garlic || insurance)) && (name != ("Drake Cula" || "Tu Fang" || "Vlad the Impaler"))
+						puts "Probably not a vampire"
 
-					elsif !(2016 - age2 == age1) && (garlic || insurance)
-					puts "Probably a vampire."
+					elsif !(2016 - year_born == age) && (garlic || insurance)
+						puts "Probably a vampire."
 
-					elsif !(2016 - age2 == age1) && !(garlic && insurance)
-					puts "Almost certainly a vampire."
+					elsif !(2016 - year_born == age) && !(garlic && insurance)
+						puts "Almost certainly a vampire."
 			
-					elsif (2016 - age2 == age1 && (garlic || insurance)) && (name == ("Drake Cula" || "Tu Fang"))
-					puts "Definitely a vampire."
+					elsif (2016 - year_born == age && (garlic || insurance)) && (name == ("Drake Cula" || "Tu Fang" || "Vlad the Impaler"))
+					puts "You're MOST DEF a vampire! Go back to your Coffin, Bloodsucker!!"
 			
 					else
 					puts "Results inconclusive"
