63baf53f07dcd568968bb08bd2bc2a38aa030991
jackshamama <shamama@gmail.com>
2016-04-25 10:52:34 -0700

    adding 5.1 excersies

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -1,39 +1,35 @@
-# Replace in the "<???>" with the appropriate method (and arguments, if any).
-# Uncomment the calls to catch these methods red-handed.
-
-# When there's more than one suspect who could have
-# committed the crime, add additional calls to prove it.
-
-# "iNvEsTiGaTiOn".<???>
+ "iNvEsTiGaTiOn".<upcase>
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+"zom".insert(1, 'o')
 # => “zoom”
 
-# "enhance".<???>
+ "enhance".center(15)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+"Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+"the usual".insert(9, " suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+" suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+"The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+"The mystery of the missing first letter".slice(1..-1)
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+"z".getbyte(0)
 # => 122 
+
 # (What is the significance of the number 122 in relation to the character z?)
+# 122 is the aplhpaneurmeric value of z in Ruby
 
 # "How many times does the letter 'a' appear in this string?".<???>
-# => 4
+# => nil (?)
